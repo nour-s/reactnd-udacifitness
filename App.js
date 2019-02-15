cbfbdef27424a4ff90af5930449fcc6a2bf61b5b
Nour Sabouny <nour.sabouny@tigerspike.com>
2019-02-16 00:56:43 +0400

    feat: Add status bar

--- App.js
+++ App.js
@@ -1,14 +1,22 @@
 import React from "react";
-import { View, Platform } from "react-native";
+import { View, Platform, StatusBar } from "react-native";
 import AddEntry from "./components/AddEntry";
 import { createStore } from "redux";
 import { Provider } from "react-redux";
 import reducer from "./reducers";
 import History from "./components/History";
-import { TabNavigator } from "react-navigation";
 import { purple, white } from "./utils/colors";
 import { FontAwesome, Ionicons } from "@expo/vector-icons";
 import { createBottomTabNavigator, createAppContainer } from "react-navigation";
+import { Constants } from "expo";
+
+function UdaciStatusBar({ backgroundColor, ...props }) {
+  return (
+    <View style={{ backgroundColor, height: Constants.statusBarHeight }}>
+      <StatusBar translucent backgroundColor={backgroundColor} {...props} />
+    </View>
+  );
+}
 
 const Tabs = createBottomTabNavigator(
   {
@@ -59,6 +67,7 @@ export default class App extends React.Component {
     return (
       <Provider store={createStore(reducer)}>
         <View style={{ flex: 1 }}>
+          <UdaciStatusBar backgroundColor={purple} barStyle="light-content" />
           <InnerApp />
         </View>
       </Provider>
