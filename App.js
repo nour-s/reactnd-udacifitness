157e29f110c1a1294336406f6512bbb1de623f3e
Nour Sabouny <nour.sabouny@tigerspike.com>
2019-02-15 18:42:22 +0400

    feat: Implement History component It will show the entries for the day or prompts for adding entry.

--- App.js
+++ App.js
@@ -1,13 +1,18 @@
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
-import AddEntry from "./components/addEntry";
+import { View } from "react-native";
+import { createStore } from "redux";
+import { Provider } from "react-redux";
+import reducer from "./reducers";
+import History from "./components/History";
 
 export default class App extends React.Component {
-	render() {
-		return (
-			<View style={{ flex: 1 }}>
-				<AddEntry />
-			</View>
-		);
-	}
+  render() {
+    return (
+      <Provider store={createStore(reducer)}>
+        <View style={{ flex: 1 }}>
+          <History />
+        </View>
+      </Provider>
+    );
+  }
 }
