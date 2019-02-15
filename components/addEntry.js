c0fdbb1249f5e299f6ea81bda67c4e0183ae38ab
Nour Sabouny <nour.sabouny@tigerspike.com>
2019-02-15 18:13:48 +0400

    feat: Styling AddEntry component

--- components/addEntry.js
+++ components/addEntry.js
@@ -1,22 +1,39 @@
 import React, { Component } from "react";
-import { View, TouchableOpacity, Text } from "react-native";
-import { getMetricMetaInfo, timeToString } from "../utils/helpers";
-import UdaciSlider from "./udaciSlider";
-import UdaciSteppers from "./udaciSteppers";
-import DateHeader from "./dateHeader";
+import {
+  View,
+  TouchableOpacity,
+  Text,
+  StyleSheet,
+  Platform
+} from "react-native";
+import {
+  getMetricMetaInfo,
+  timeToString,
+  getDailyReminderValue
+} from "../utils/helpers";
+import UdaciSlider from "./UdaciSlider";
+import UdaciSteppers from "./UdaciSteppers";
+import DateHeader from "./DateHeader";
 import { Ionicons } from "@expo/vector-icons";
-import TextButton from "./textButton";
+import TextButton from "./TextButton";
 import { submitEntry, removeEntry } from "../utils/api";
+import { connect } from "react-redux";
+import { addEntry } from "../actions";
+import { purple, white } from "../utils/colors";
 
 function SubmitBtn({ onPress }) {
   return (
-    <TouchableOpacity onPress={onPress}>
-      <Text>SUBMIT</Text>
+    <TouchableOpacity
+      style={
+        Platform.OS === "ios" ? styles.iosSubmitBtn : styles.AndroidSubmitBtn
+      }
+      onPress={onPress}
+    >
+      <Text style={styles.submitBtnText}>SUBMIT</Text>
     </TouchableOpacity>
   );
 }
-
-export default class AddEntry extends Component {
+class AddEntry extends Component {
   state = {
     run: 0,
     bike: 0,
@@ -55,7 +72,11 @@ export default class AddEntry extends Component {
     const key = timeToString();
     const entry = this.state;
 
-    // Update Redux
+    this.props.dispatch(
+      addEntry({
+        [key]: entry
+      })
+    );
 
     this.setState(() => ({ run: 0, bike: 0, swim: 0, sleep: 0, eat: 0 }));
 
@@ -68,7 +89,11 @@ export default class AddEntry extends Component {
   reset = () => {
     const key = timeToString();
 
-    // Update Redux
+    this.props.dispatch(
+      addEntry({
+        [key]: getDailyReminderValue()
+      })
+    );
 
     // Route to Home
 
@@ -79,23 +104,28 @@ export default class AddEntry extends Component {
 
     if (this.props.alreadyLogged) {
       return (
-        <View>
-          <Ionicons name={"ios-happy-outline"} size={100} />
+        <View style={styles.center}>
+          <Ionicons
+            name={Platform.OS === "ios" ? "ios-happy" : "md-happy"}
+            size={100}
+          />
           <Text>You already logged your information for today.</Text>
-          <TextButton onPress={this.reset}>Reset</TextButton>
+          <TextButton style={{ padding: 10 }} onPress={this.reset}>
+            Reset
+          </TextButton>
         </View>
       );
     }
 
     return (
-      <View>
+      <View style={styles.container}>
         <DateHeader date={new Date().toLocaleDateString()} />
         {Object.keys(metaInfo).map(key => {
           const { getIcon, type, ...rest } = metaInfo[key];
           const value = this.state[key];
 
           return (
-            <View key={key}>
+            <View key={key} style={styles.row}>
               {getIcon()}
               {type === "slider" ? (
                 <UdaciSlider
@@ -104,13 +134,13 @@ export default class AddEntry extends Component {
                   {...rest}
                 />
               ) : (
-                  <UdaciSteppers
-                    value={value}
-                    onIncrement={() => this.increment(key)}
-                    onDecrement={() => this.decrement(key)}
-                    {...rest}
-                  />
-                )}
+                <UdaciSteppers
+                  value={value}
+                  onIncrement={() => this.increment(key)}
+                  onDecrement={() => this.decrement(key)}
+                  {...rest}
+                />
+              )}
             </View>
           );
         })}
@@ -119,3 +149,57 @@ export default class AddEntry extends Component {
     );
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 20,
+    backgroundColor: white
+  },
+  row: {
+    flexDirection: "row",
+    flex: 1,
+    alignItems: "center"
+  },
+  iosSubmitBtn: {
+    backgroundColor: purple,
+    padding: 10,
+    borderRadius: 7,
+    height: 45,
+    marginLeft: 40,
+    marginRight: 40
+  },
+  AndroidSubmitBtn: {
+    backgroundColor: purple,
+    padding: 10,
+    paddingLeft: 30,
+    paddingRight: 30,
+    height: 45,
+    borderRadius: 2,
+    alignSelf: "flex-end",
+    justifyContent: "center",
+    alignItems: "center"
+  },
+  submitBtnText: {
+    color: white,
+    fontSize: 22,
+    textAlign: "center"
+  },
+  center: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    marginLeft: 30,
+    marginRight: 30
+  }
+});
+
+function mapStateToProps(state) {
+  const key = timeToString();
+
+  return {
+    alreadyLogged: state[key] && typeof state[key].today === "undefined"
+  };
+}
+
+export default connect(mapStateToProps)(AddEntry);
