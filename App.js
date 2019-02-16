e4b770d83dd84747ee394f6c546a57017c22b0a9
Nour Sabouny <nour.sabouny@tigerspike.com>
2019-02-17 00:52:41 +0400

    feat: Sent local notification when submitting new entry for the next day

--- App.js
+++ App.js
@@ -9,106 +9,130 @@ import History from "./components/History";
 import { purple, white } from "./utils/colors";
 import { FontAwesome, Ionicons } from "@expo/vector-icons";
 import {
-  createBottomTabNavigator,
-  createAppContainer,
-  createStackNavigator
+	createBottomTabNavigator,
+	createAppContainer,
+	createStackNavigator
 } from "react-navigation";
 import { Constants } from "expo";
 import Live from "./components/Live";
+import { setLocalNotification } from "./utils/helpers";
 
 function UdaciStatusBar({ backgroundColor, ...props }) {
-  return (
-    <View style={{ backgroundColor, height: Constants.statusBarHeight }}>
-      <StatusBar translucent backgroundColor={backgroundColor} {...props} />
-    </View>
-  );
+	return (
+		<View style={{ backgroundColor, height: Constants.statusBarHeight }}>
+			<StatusBar
+				translucent
+				backgroundColor={backgroundColor}
+				{...props}
+			/>
+		</View>
+	);
 }
 
 const Tabs = createBottomTabNavigator(
-  {
-    History: {
-      screen: History,
-      navigationOptions: {
-        tabBarLabel: "History",
-        tabBarIcon: ({ tintColor }) => (
-          <Ionicons name="ios-bookmarks" size={30} color={tintColor} />
-        )
-      }
-    },
-    AddEntry: {
-      screen: AddEntry,
-      navigationOptions: {
-        tabBarLabel: "Add Entry",
-        tabBarIcon: ({ tintColor }) => (
-          <FontAwesome name="plus-square" size={30} color={tintColor} />
-        )
-      }
-    },
-    Live: {
-      screen: Live,
-      navigationOptions: {
-        tabBarLabel: "Live",
-        tabBarIcon: ({ tintColor }) => (
-          <Ionicons name="ios-speedometer" size={30} color={tintColor} />
-        )
-      }
-    }
-  },
-  {
-    navigationOptions: {
-      header: null
-    },
-    tabBarOptions: {
-      // The color of the icon on the navigation bar.
-      activeTintColor: Platform.OS === "ios" ? purple : white,
-      style: {
-        height: 56,
-        // The background behind the navigation icon
-        backgroundColor: Platform.OS === "ios" ? white : purple,
-        shadowColor: "rgba(0, 0, 0, 0.24)",
-        shadowOffset: {
-          width: 0,
-          height: 3
-        },
-        shadowRadius: 6,
-        shadowOpacity: 1
-      }
-    }
-  }
+	{
+		History: {
+			screen: History,
+			navigationOptions: {
+				tabBarLabel: "History",
+				tabBarIcon: ({ tintColor }) => (
+					<Ionicons
+						name="ios-bookmarks"
+						size={30}
+						color={tintColor}
+					/>
+				)
+			}
+		},
+		AddEntry: {
+			screen: AddEntry,
+			navigationOptions: {
+				tabBarLabel: "Add Entry",
+				tabBarIcon: ({ tintColor }) => (
+					<FontAwesome
+						name="plus-square"
+						size={30}
+						color={tintColor}
+					/>
+				)
+			}
+		},
+		Live: {
+			screen: Live,
+			navigationOptions: {
+				tabBarLabel: "Live",
+				tabBarIcon: ({ tintColor }) => (
+					<Ionicons
+						name="ios-speedometer"
+						size={30}
+						color={tintColor}
+					/>
+				)
+			}
+		}
+	},
+	{
+		navigationOptions: {
+			header: null
+		},
+		tabBarOptions: {
+			// The color of the icon on the navigation bar.
+			activeTintColor: Platform.OS === "ios" ? purple : white,
+			style: {
+				height: 56,
+				// The background behind the navigation icon
+				backgroundColor: Platform.OS === "ios" ? white : purple,
+				shadowColor: "rgba(0, 0, 0, 0.24)",
+				shadowOffset: {
+					width: 0,
+					height: 3
+				},
+				shadowRadius: 6,
+				shadowOpacity: 1
+			}
+		}
+	}
 );
 
 const MainNavigator = createStackNavigator({
-  // The order decides which one is the first shown.
-  Home: {
-    screen: Tabs,
-    navigationOptions: {
-      header: null
-    }
-  },
-  EntryDetail: {
-    screen: EntryDetail,
-    navigationOptions: ({ navigation }) => ({
-      // the color of the 'back' arrow.
-      headerTintColor: white,
-      headerStyle: {
-        // The background color behind the 'back' arrow.
-        backgroundColor: "purple"
-      }
-    })
-  }
+	// The order decides which one is the first shown.
+	Home: {
+		screen: Tabs,
+		navigationOptions: {
+			header: null
+		}
+	},
+	EntryDetail: {
+		screen: EntryDetail,
+		navigationOptions: ({ navigation }) => ({
+			// the color of the 'back' arrow.
+			headerTintColor: white,
+			headerStyle: {
+				// The background color behind the 'back' arrow.
+				backgroundColor: "purple"
+			}
+		})
+	}
 });
 
 const InnerApp = createAppContainer(MainNavigator);
 
 export default class App extends React.Component {
-  render() {
-    return (
-      <Provider store={createStore(reducer)}>
-        <View style={{ flex: 1 }}>
-          <UdaciStatusBar backgroundColor={purple} barStyle="light-content" />
-          <InnerApp />
-        </View>
-      </Provider>
-    );
-  }
+	componentDidMount() {
+		setLocalNotification();
+	}
+
+	render() {
+		return (
+			<Provider store={createStore(reducer)}>
+				<View style={{ flex: 1 }}>
+					<UdaciStatusBar
+						backgroundColor={purple}
+						barStyle="light-content"
+					/>
+					<InnerApp />
+				</View>
+			</Provider>
+		);
+	}
 }
