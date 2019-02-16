5265418a69f21385e7f47a4647e1e6d49504c2d6
Nour Sabouny <nour.sabouny@tigerspike.com>
2019-02-16 12:54:34 +0400

    feat: Add a title to the EntryDetail Using the static navigationOptions method inside EntryDetail

--- components/EntryDetail.js
+++ components/EntryDetail.js
@@ -2,11 +2,24 @@ import React, { Component } from "react";
 import { View, Text } from "react-native";
 
 class EntryDetail extends Component {
+  // This method is called when the stack navigator is about to render this component.
+  static navigationOptions = ({ navigation }) => {
+    const { entryId } = navigation.state.params;
+
+    const year = entryId.slice(0, 4);
+    const month = entryId.slice(5, 7);
+    const day = entryId.slice(8);
+    return {
+      title: `${month}/${day}/${year}`
+    };
+  };
+
   render() {
     return (
       <View>
         <Text>
-          Entry Detail - {JSON.stringify(this.props.navigation.state.params.entryId)}
+          Entry Detail -{" "}
+          {JSON.stringify(this.props.navigation.state.params.entryId)}
         </Text>
       </View>
     );
