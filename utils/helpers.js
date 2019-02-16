525dc9e2eb059d5a0ebabed2b65d7076b50c54b7
Nour Sabouny <nour.sabouny@tigerspike.com>
2019-02-17 00:24:08 +0400

    feat: Adding helpers methods for Notifications

--- utils/helpers.js
+++ utils/helpers.js
@@ -1,12 +1,19 @@
-import React from 'react'
-import { View, StyleSheet } from 'react-native'
-import { FontAwesome, MaterialIcons, MaterialCommunityIcons } from '@expo/vector-icons'
-import { red, orange, blue, lightPurp, pink, white } from './colors'
+import React from "react";
+import { View, StyleSheet, AsyncStorage } from "react-native";
+import {
+	FontAwesome,
+	MaterialIcons,
+	MaterialCommunityIcons
+} from "@expo/vector-icons";
+import { red, orange, blue, lightPurp, pink, white } from "./colors";
+import { Notifications, Permissions } from "expo";
+
+const NOTIFICATION_KEY = "UdaciFitness:notifications";
 
 export function getDailyReminderValue() {
 	return {
 		today: "👋 Don't forget to log your data today!"
-	}
+	};
 }
 
 const styles = StyleSheet.create({
@@ -15,150 +22,223 @@ const styles = StyleSheet.create({
 		borderRadius: 8,
 		width: 50,
 		height: 50,
-		justifyContent: 'center',
-		alignItems: 'center',
+		justifyContent: "center",
+		alignItems: "center",
 		marginRight: 20
-	},
-})
+	}
+});
 
 export function getMetricMetaInfo(metric) {
 	const info = {
 		run: {
-			displayName: 'Run',
+			displayName: "Run",
 			max: 50,
-			unit: 'miles',
+			unit: "miles",
 			step: 1,
-			type: 'steppers',
+			type: "steppers",
 			getIcon() {
 				return (
-					<View style={[styles.iconContainer, { backgroundColor: red }]}>
+					<View
+						style={[styles.iconContainer, { backgroundColor: red }]}
+					>
 						<MaterialIcons
-							name='directions-run'
+							name="directions-run"
 							color={white}
 							size={35}
 						/>
 					</View>
-				)
+				);
 			}
 		},
 		bike: {
-			displayName: 'Bike',
+			displayName: "Bike",
 			max: 100,
-			unit: 'miles',
+			unit: "miles",
 			step: 1,
-			type: 'steppers',
+			type: "steppers",
 			getIcon() {
 				return (
-					<View style={[styles.iconContainer, { backgroundColor: orange }]}>
+					<View
+						style={[
+							styles.iconContainer,
+							{ backgroundColor: orange }
+						]}
+					>
 						<MaterialCommunityIcons
-							name='bike'
+							name="bike"
 							color={white}
 							size={32}
 						/>
 					</View>
-				)
+				);
 			}
 		},
 		swim: {
-			displayName: 'Swim',
+			displayName: "Swim",
 			max: 9900,
-			unit: 'meters',
+			unit: "meters",
 			step: 100,
-			type: 'steppers',
+			type: "steppers",
 			getIcon() {
 				return (
-					<View style={[styles.iconContainer, { backgroundColor: blue }]}>
+					<View
+						style={[
+							styles.iconContainer,
+							{ backgroundColor: blue }
+						]}
+					>
 						<MaterialCommunityIcons
-							name='swim'
+							name="swim"
 							color={white}
 							size={35}
 						/>
 					</View>
-				)
+				);
 			}
 		},
 		sleep: {
-			displayName: 'Sleep',
+			displayName: "Sleep",
 			max: 24,
-			unit: 'hours',
+			unit: "hours",
 			step: 1,
-			type: 'slider',
+			type: "slider",
 			getIcon() {
 				return (
-					<View style={[styles.iconContainer, { backgroundColor: lightPurp }]}>
-						<FontAwesome
-							name='bed'
-							color={white}
-							size={30}
-						/>
+					<View
+						style={[
+							styles.iconContainer,
+							{ backgroundColor: lightPurp }
+						]}
+					>
+						<FontAwesome name="bed" color={white} size={30} />
 					</View>
-				)
+				);
 			}
 		},
 		eat: {
-			displayName: 'Eat',
+			displayName: "Eat",
 			max: 10,
-			unit: 'rating',
+			unit: "rating",
 			step: 1,
-			type: 'slider',
+			type: "slider",
 			getIcon() {
 				return (
-					<View style={[styles.iconContainer, { backgroundColor: pink }]}>
+					<View
+						style={[
+							styles.iconContainer,
+							{ backgroundColor: pink }
+						]}
+					>
 						<MaterialCommunityIcons
-							name='food'
+							name="food"
 							color={white}
 							size={35}
 						/>
 					</View>
-				)
+				);
 			}
-		},
-	}
+		}
+	};
 
-	return typeof metric === 'undefined'
-		? info
-		: info[metric]
+	return typeof metric === "undefined" ? info : info[metric];
 }
-
 
 export function isBetween(num, x, y) {
 	if (num >= x && num <= y) {
-		return true
+		return true;
 	}
 
-	return false
+	return false;
 }
 
 export function calculateDirection(heading) {
-	let direction = ''
+	let direction = "";
 
 	if (isBetween(heading, 0, 22.5)) {
-		direction = 'North'
+		direction = "North";
 	} else if (isBetween(heading, 22.5, 67.5)) {
-		direction = 'North East'
+		direction = "North East";
 	} else if (isBetween(heading, 67.5, 112.5)) {
-		direction = 'East'
+		direction = "East";
 	} else if (isBetween(heading, 112.5, 157.5)) {
-		direction = 'South East'
+		direction = "South East";
 	} else if (isBetween(heading, 157.5, 202.5)) {
-		direction = 'South'
+		direction = "South";
 	} else if (isBetween(heading, 202.5, 247.5)) {
-		direction = 'South West'
+		direction = "South West";
 	} else if (isBetween(heading, 247.5, 292.5)) {
-		direction = 'West'
+		direction = "West";
 	} else if (isBetween(heading, 292.5, 337.5)) {
-		direction = 'North West'
+		direction = "North West";
 	} else if (isBetween(heading, 337.5, 360)) {
-		direction = 'North'
+		direction = "North";
 	} else {
-		direction = 'Calculating'
+		direction = "Calculating";
 	}
 
-	return direction
+	return direction;
 }
 
 export function timeToString(time = Date.now()) {
-	const date = new Date(time)
-	const todayUTC = new Date(Date.UTC(date.getFullYear(), date.getMonth(), date.getDate()))
-	return todayUTC.toISOString().split('T')[0]
+	const date = new Date(time);
+	const todayUTC = new Date(
+		Date.UTC(date.getFullYear(), date.getMonth(), date.getDate())
+	);
+	return todayUTC.toISOString().split("T")[0];
+}
+
+export function clearLocalNotification() {
+	return AsyncStorage.removeItem(NOTIFICATION_KEY).then(
+		Notifications.cancelAllScheduledNotificationsAsync
+	);
+}
+
+function createNotification() {
+	return {
+		title: "Log your stats!",
+		body: "👋 don't forget to log your stats for today!",
+		ios: {
+			sound: true
+		},
+		android: {
+			sound: true,
+			priority: "high",
+			sticky: false,
+			vibrate: true
+		}
+	};
+}
+
+export function setLocalNotification() {
+	AsyncStorage.getItem(NOTIFICATION_KEY)
+		.then(JSON.parse)
+		.then(data => {
+			if (data === null) {
+				Permissions.askAsync(Permissions.NOTIFICATIONS).then(
+					({ status }) => {
+						if (status === "granted") {
+							Notifications.cancelAllScheduledNotificationsAsync();
+
+							let tomorrow = new Date();
+							tomorrow.setDate(tomorrow.getDate() + 1);
+							tomorrow.setHours(20);
+							tomorrow.setMinutes(0);
+
+							Notifications.scheduleLocalNotificationAsync(
+								createNotification(),
+								{
+									time: tomorrow,
+									repeat: "day"
+								}
+							);
+
+							AsyncStorage.setItem(
+								NOTIFICATION_KEY,
+								JSON.stringify(true)
+							);
+						}
+					}
+				);
+			}
+		});
 }
